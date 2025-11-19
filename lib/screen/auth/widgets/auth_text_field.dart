56955b7edfbb27f416b5b2cc40b340f5e8ef5409
Nisrina Asad <nisrina@nisrina-mekbuk.local>
2025-11-19 10:28:22 +0700

    finishin login screen ui definition

--- lib/screen/auth/widgets/auth_text_field.dart
+++ lib/screen/auth/widgets/auth_text_field.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class AuthTextField extends StatelessWidget {
   final TextEditingController controller;
   final String label;
-  final IconData icon;
+  final IconData? icon;
   final bool obscureText; // si password itu, yg bulet" itu
   final Widget? suffixIcon;
   final TextInputType? keyboardType; // yg ada @ sama angka -> password and email
   final String? Function(String?)? validator; // utk error yang di atas text field nya
 
-  const AuthTextField({super.key, required this.controller, required this.label, required this.icon, required this.obscureText, this.suffixIcon, this.keyboardType, this.validator});
+  const AuthTextField({super.key, required this.controller, required this.label, this.icon, required this.obscureText, this.suffixIcon, this.keyboardType, this.validator});
 
   @override
   Widget build(BuildContext context) {
